6b82f091c71b6554450b1f0a5301ff8aed930c7d
Raihanul Haque <raihanulhaque@rocketmail.com>
2020-09-26 16:50:16 +0600

    signIn component setup along with CustomButton comp

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import ShopPage from './pages/shop/Shop';
 import Header from './components/header/Header';
+import SignInSignUp from './pages/sign-in-sign-up/SignInSignUp';
 
 import Homepage from './pages/homepage/Homepage';
 
@@ -13,6 +14,7 @@ function App() {
     <Switch>
       <Route exact path='/' component={Homepage} />
       <Route path='/shop' component={ShopPage} />
+      <Route path='/signin' component={SignInSignUp} />
     </Switch>
     </div>
   );
---
+++ src/pages/sign-in-sign-up/SignInSignUp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './sign-in-sign-up.scss';
+import SignIn from '../../components/sign-in/SignIn';
+
+function SignInSignUp() {
+    return (
+        <div>
+            <SignIn />
+        </div>
+    )
+}
+
+export default SignInSignUp;
