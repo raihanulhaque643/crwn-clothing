fb11489452bc1a1927a10f9be5058fca93dfc241
Raihanul Haque <raihanulhaque@rocketmail.com>
2020-09-26 15:49:33 +0600

    header compoenent

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import ShopPage from './pages/shop/Shop';
+import Header from './components/header/Header';
 
 import Homepage from './pages/homepage/Homepage';
 
 function App() {
   return (
     <div>
+    <Header />
     <Switch>
       <Route exact path='/' component={Homepage} />
       <Route path='/shop' component={ShopPage} />
