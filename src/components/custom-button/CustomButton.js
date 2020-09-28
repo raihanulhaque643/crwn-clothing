6d445d3df4edd3bd44072d83829ce28c2596d7b2
Raihanul Haque <raihanulhaque@rocketmail.com>
2020-09-28 18:57:27 +0600

    cart item reducer

--- src/components/custom-button/CustomButton.js
+++ src/components/custom-button/CustomButton.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './custom-button.scss';
 
-function CustomButton({ children, isGoogleSignIn, ...otherProps }) {
+function CustomButton({ children, isGoogleSignIn, inverted, ...otherProps }) {
     return (
         <button
-        className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
+        className={`${inverted ? 'inverted' : ''} ${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
         {...otherProps}
         >
         {children}
