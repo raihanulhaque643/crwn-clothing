a33ab8bc02f979e18a894c9382c13d3fdb9bb606
Raihanul Haque <raihanulhaque@rocketmail.com>
2020-09-29 16:17:33 +0600

    component level states of Directory comp and Shop page comp moved to redux store

--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -1,28 +1,22 @@
-import React, { Component } from 'react';
-import SHOP_DATA from './ShopData';
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
 import CollectionPreview from '../../components/collection-preview/CollectionPreview';
+import { selectCollections } from '../../redux/shop/shop-selectors';
 
-class ShopPage extends Component {
-    constructor(props) {
-        super(props);
-        
-        this.state = {
-            collections: SHOP_DATA
+const ShopPage = ({ collections }) => (
+    <div className="shop-page">
+        {
+            collections.map(({id, ...otherCollectionProps}) => (
+                <CollectionPreview key={id} {...otherCollectionProps} />
+            ))
         }
-    }
+    </div>
+);
 
-    render() {
-        const {collections} = this.state;
-        return (
-            <div className="shop-page">
-                {
-                    collections.map(({id, ...otherCollectionProps}) => (
-                        <CollectionPreview key={id} {...otherCollectionProps} />
-                    ))
-                }
-            </div>
-        )
-    }
-}
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollections
+});
 
-export default ShopPage;
+export default connect(mapStateToProps)(ShopPage);
